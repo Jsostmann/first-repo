3161eeb17cfcd3491669082463ed0b4d2f0917a6
James Ostmann <42648820+Jsostmann@users.noreply.github.com>
2018-11-18 20:17:39 -0500

    Update Goodbye163.java

--- Goodbye163.java
+++ Goodbye163.java
@@ -1,7 +1,7 @@
+
 /**
 * @author jsostmann@aggies.ncat.edu
 * Class that prints out Goodbye Comp 163 to console.
-*
 */
 
 public class Goodbye163 {
@@ -9,14 +9,10 @@ public class Goodbye163 {
 /**
 * Main Method of Goodbye 163
 * @param args supplied command line arguments.
-*
 */
-
+ 
  public static void main(String[]args) {
- 
   System.out.println("Goodbye Comp 163.");
- 
  }
-
-
+ 
 }
