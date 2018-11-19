e8a1743f3bb9514e44bc17aedc94685337b63dd6
James Ostmann <jsostmann@aggies.ncat.edu>
2018-11-18 20:11:50 -0500

    first repo commit.

---
+++ Goodbye163.java
@@ -0,0 +1,22 @@
+/**
+* @author jsostmann@aggies.ncat.edu
+* Class that prints out Goodbye Comp 163 to console.
+*
+*/
+
+public class Goodbye163 {
+
+/**
+* Main Method of Goodbye 163
+* @param args supplied command line arguments.
+*
+*/
+
+ public static void main(String[]args) {
+ 
+  System.out.println("Goodbye Comp 163.");
+ 
+ }
+
+
+}
